b7326567895c694dcad7e7ff06c885bd507a2510
jakubminka <jakubminka@gmail.com>
2026-03-23 17:24:06 +0100

    Update supabaseClient.ts

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://ysnwurduyfgymxtcfnhp.supabase.co';
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlzbnd1cmR1eWZneW14dGNmbmhwIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc3MDMxMjgxNCwiZXhwIjoyMDg1ODg4ODE0fQ.mn9GcNlvrbd_PH2oeTZnVo6JIVL7ydhK9JAxZOpJGIE';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || 'https://vpsskbxtpqtotlyzszhy.supabase.co';
+const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inlwb3N0dmNyeWNhdGVwbG9ldCIsInJvbGUiOiJzZXJ2aWNlX3JvbGUiLCJpYXQiOjE3NzAzMTI4MjEsImV4cCI6MjA4NTg4ODgxMX0.XXXX';
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
